4c622c998af7da26a3ee5dc708b13fee669ef4de
luukje1 <137725781+luukje1@users.noreply.github.com>
2023-06-29 19:23:29 +0200

    Initial commit

---
+++ resources/js/Pages/Show.vue
@@ -0,0 +1,27 @@
+<script setup>
+import KinstaLayout from "../Layouts/KinstaLayout.vue";
+import { Link } from "@inertiajs/inertia-vue3";
+
+defineProps({
+    article: Object,
+});
+</script>
+
+<template>
+    <KinstaLayout>
+        <article class="mx-auto mt-10 flex justify-center max-w-5xl border-b-2">
+            <img
+                src="/images/kinsta-logo1.png"
+                class="w-80 h-80 rounded-xl mx-auto py-5"
+                alt=""
+            />
+            <div class="text-left flex flex-col pt-5 pb-10 px-10">
+                <h1 class="text-xl font-semibold mb-10">{{ article.title }}</h1>
+                <p>{{ article.body }}</p>
+                <Link :href="route('home')"
+                class="text-indigo-600 hover:text-indigo-800 w-fit self-end font-semibold"
+                >Back</Link>
+            </div>
+        </article>
+    </KinstaLayout>
+</template>
